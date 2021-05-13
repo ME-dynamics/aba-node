4bc18df25e674b571179b23538b7a6122f649a70
eyousefifar <eyousefifar@gmail.com>
2021-05-13 18:20:37 +0430

    add query option support

--- src/scylla-client/select.ts
+++ src/scylla-client/select.ts
@@ -1,11 +1,10 @@
 import { ErrorFactory } from "aba-utils";
 import { IBuildDbFunc, ISelect, tResultSet } from "../types";
 
-
 /**
  ** builds select function for selecting rows, also check uniqueness if needed
  * @param args object containing cassandra driver client instance
- * @returns  async function upsert 
+ * @returns  async function upsert
  */
 export function buildSelect(args: IBuildDbFunc) {
   const { client } = args;
@@ -14,14 +13,28 @@ export function buildSelect(args: IBuildDbFunc) {
    ** check uniqueness. meaning there should be only one result per query ( for each partition key )
    ** if not will result and Error Factory.
    * !remember if this happens in code. it's a design flaw and should be
-   * !fixed immediately. must be caught on testing   
+   * !fixed immediately. must be caught on testing
    * @param info object containing query string, params, unique and error path
-   * @returns scylla db result set 
+   * @returns scylla db result set
    */
   return async function select(info: ISelect): Promise<tResultSet> {
-    const { query, params, unique, errorPath } = info;
+    const { query, params, unique, queryOptions, errorPath } = info;
+    const {
+      autoPage,
+      consistency,
+      fetchSize,
+      pageState,
+      serialConsistency,
+    } = queryOptions;
     try {
-      const result = await client.execute(query, params, { prepare: true });
+      const result = await client.execute(query, params, {
+        prepare: true,
+        autoPage,
+        fetchSize,
+        pageState,
+        consistency,
+        serialConsistency,
+      });
       // check if row is unique
       if (unique && result.rowLength > 1) {
         throw new ErrorFactory({
