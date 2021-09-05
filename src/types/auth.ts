e3f34e8b7205a1d931f23b3f3de7b6de0999c3ed
eyousefifar <eyousefifar@gmail.com>
2021-09-05 21:27:16 +0430

    auth types

---
+++ src/types/auth.ts
@@ -0,0 +1,10 @@
+
+
+export interface IRoles {
+  admin: boolean;
+  provider: boolean;
+  assistant: boolean;
+  customer: boolean;
+  support: boolean;
+  accountant: boolean;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+export * from "./auth";
+
 export * from "./httpResult";
 
 export * from "./randomDigits";
