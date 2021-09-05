3f2bd3d7635b2025dfd3dc32fee1f8efae391784
eyousefifar <eyousefifar@gmail.com>
2021-09-05 22:09:01 +0430

    add fastify instance type

--- src/types/http.ts
+++ src/types/http.ts
@@ -1,11 +1,13 @@
-import { FastifyRequest, FastifyReply } from "fastify";
+import { FastifyRequest, FastifyReply, FastifyInstance } from "fastify";
 
 export type tRequest<T> = FastifyRequest<T>;
 // http interfaces start
 export type tReply = FastifyReply;
 // http interfaces end
 
+export type tHttpInstance = FastifyInstance;
 
 export interface IHttpClient {
     dev: boolean;
 }
+
