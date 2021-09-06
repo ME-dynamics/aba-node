948a4ed1d400cd2dff780c4e7fb8915d1b3a97fc
eyousefifar <eyousefifar@gmail.com>
2021-09-06 19:59:42 +0430

    change to result function for unified interface

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,3 +1,4 @@
+import { result } from "aba-utils";
 import { httpResultClientError } from "../http-result";
 import { tRequest, IRoles } from "../types";
 
@@ -6,39 +7,83 @@ const { unauthorized } = httpResultClientError;
 export function auth<T>(httpRequest: tRequest<T>, roles: IRoles) {
   const jwtPayload = httpRequest.headers["x-jwt-payload"];
   if (typeof jwtPayload !== "string") {
-    return unauthorized({ error: "jwt is not defined" });
+    return result({
+      success: false,
+      error: "jwt is not defined",
+      payload: unauthorized({ error: "jwt is not defined" }),
+    });
   }
   const jwt = JSON.parse(Buffer.from(jwtPayload, "base64").toString());
   // check if jwt is expired
   if (typeof jwt["exp"] !== "number") {
-    return unauthorized({ error: "invalid jwt" });
+    return result({
+      success: false,
+      error: "invalid jwt",
+      payload: unauthorized({ error: "invalid jwt" }),
+    });
   }
   if (jwt["exp"] < Date.now() / 1000) {
-    return unauthorized({ error: "jwt expired" });
+    return result({
+      success: false,
+      error: "jwt expired",
+      payload: unauthorized({ error: "jwt expired" }),
+    });
   }
   // check if jwt has id
   if (typeof jwt["sub"] !== "string") {
-    return unauthorized({ error: "invalid jwt" });
+    return result({
+      success: false,
+      error: "invalid jwt",
+      payload: unauthorized({ error: "invalid jwt" }),
+    });
   }
   const { admin, accountant, assistant, customer, provider, support } = roles;
   // if admin, can access all the routes
   if (admin && jwt["admin"]) {
-    return { userId: jwt["sub"] };
+    return result({
+      success: true,
+      error: undefined,
+      payload: { userId: jwt["sub"] },
+    });
   }
   if (provider && jwt["provider"]) {
-    return { userId: jwt["sub"] };
+    return result({
+      success: true,
+      error: undefined,
+      payload: { userId: jwt["sub"] },
+    });
   }
   if (customer && jwt["customer"]) {
-    return { userId: jwt["sub"] };
+    return result({
+      success: true,
+      error: undefined,
+      payload: { userId: jwt["sub"] },
+    });
   }
   if (assistant && jwt["assistant"]) {
-    return { userId: jwt["sub"] };
+    return result({
+      success: true,
+      error: undefined,
+      payload: { userId: jwt["sub"] },
+    });
   }
   if (accountant && jwt["accountant"]) {
-    return { userId: jwt["sub"] };
+    return result({
+      success: true,
+      error: undefined,
+      payload: { userId: jwt["sub"] },
+    });
   }
   if (support && jwt["support"]) {
-    return { userId: jwt["sub"] };
+    return result({
+      success: true,
+      error: undefined,
+      payload: { userId: jwt["sub"] },
+    });
   }
-  return unauthorized({ error: "unauthorized" });
+  return result({
+    success: false,
+    error: "unauthorized",
+    payload: unauthorized({ error: "unauthorized" }),
+  });
 }
