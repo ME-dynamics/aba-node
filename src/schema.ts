8fadac302616cf161a5c390c229a2a3249e6c037
eyousefifar <eyousefifar@gmail.com>
2022-02-01 13:57:22 +0330

    export general error schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,11 @@
 import schema from "fluent-json-schema";
 
-
 export const fluentSchema = schema;
-export const errorSchema = schema.object().prop("error", schema.string().required().minLength(1).maxLength(577))
+export const errorSchema = schema
+  .object()
+  .prop("error", schema.string().required().minLength(1).maxLength(577));
+
+export const errorSchemaObject = {
+  "4xx": errorSchema,
+  "5xx": errorSchema,
+};
