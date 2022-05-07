6fdb4c67059ddf52051698c94f69cee44d3fdea8
eyousefifar <eyousefifar@gmail.com>
2022-05-07 14:10:33 +0430

    ttl test in update query

--- __tests__/update.ts
+++ __tests__/update.ts
@@ -35,4 +35,24 @@ describe("update query", () => {
     expect(query.query.replace("\n", "")).toBe(expectedQuery);
     expect(query.logQuery.replace("\n", "")).toBe(expectedLogQuery);
   });
+  it("should return query with ttl", () => {
+    const query = updateQuery({
+      table: "user",
+      version: "v1",
+      values: [
+        { column: "name", dynamicValue: true },
+        { column: "age", staticValue: 20 },
+      ],
+      where: ["id = 1"],
+      lwt: ["gender = 'male'"],
+      ttl: {
+        hours: 1
+      },
+      logIdLabel: ["id"],
+    });
+    const expectedQuery = `UPDATE user_v1 USING TTL 3600 SET name = :name, age = 20 WHERE id = 1 IF gender = 'male';`;
+    const expectedLogQuery = `INSERT INTO user_v1_log (id, dml, name, age) VALUES (:id, 'update', :name, 20);`;
+    expect(query.query.replace("\n", "")).toBe(expectedQuery);
+    expect(query.logQuery.replace("\n", "")).toBe(expectedLogQuery);
+  })
 });
