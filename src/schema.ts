9bd231391ac543c72112650c2daed6bd6b113986
eyousefifar <eyousefifar@gmail.com>
2023-04-30 20:18:49 +0330

    remove schema

--- src/schema.ts
+++
@@ -1,5 +0,0 @@
-import schema from "fluent-json-schema";
-
-
-export const fluentSchema = schema;
-export const errorSchema = schema.object().prop("error", schema.string().required().minLength(1).maxLength(577))
