9b8daee3d1cb124efd3cbaf10d9ae9e28168922e
eyousefifar <eyousefifar@gmail.com>
2023-05-02 17:50:07 +0330

    remove deleted packages

--- index.ts
+++ index.ts
@@ -4,15 +4,6 @@ export * as types from "./src/types";
 export { secureRandomNumber } from "./src/random-digits";
 export * from "./src/http-result";
 
-export { scyllaClient, dataType } from "./src/scylla-client";
-
-export * as queryGen from "./src/query";
-
 export { StatusCodes as statusCodes } from "http-status-codes";
 
-export { httpClient } from "./src/http-client";
-
 export * from "aba-utils";
-export { pump } from "./src/pump";
-
-export { errorSchema, fluentSchema } from "./src/schema";
