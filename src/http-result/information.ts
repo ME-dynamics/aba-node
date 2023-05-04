87df62fd38c49fe1786374201ac325d6521fd6e6
eyousefifar <eyousefifar@gmail.com>
2023-05-04 14:18:52 +0330

    add information http result

--- src/http-result/information.ts
+++ src/http-result/information.ts
@@ -1,7 +1,31 @@
+import { StatusCodes } from "http-status-codes";
+import type { IErrorResult, IReqError } from "../types";
 
+/**
+ * Returns an error object containing an error code and error string for HTTP information responses.
+ * @param args - An object that contains request error text.
+ * @param code - HTTP status code.
+ * @returns An object containing error number and error string.
+ */
+function information(args: IReqError, code: number): IErrorResult {
+  const { error } = args;
+  if (!error) throw new Error("HTTP: information error must be defined");
+  return {
+    code,
+    error,
+    payload: undefined,
+  };
+}
 
+export function continueRequest(args: IReqError): IErrorResult {
+  return information(args, StatusCodes.CONTINUE);
+}
 
-// nothing to implement yet
+export function switchingProtocols(args: IReqError): IErrorResult {
+  return information(args, StatusCodes.SWITCHING_PROTOCOLS);
+}
 
-
+export function processing(args: IReqError): IErrorResult {
+  return information(args, StatusCodes.PROCESSING);
+}
 
