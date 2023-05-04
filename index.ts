12f0de9086cd106158270a0d59840b08397d9525
eyousefifar <eyousefifar@gmail.com>
2023-05-04 14:19:32 +0330

    export route gen and logger

--- index.ts
+++ index.ts
@@ -5,7 +5,8 @@ export { auth } from "./src/auth";
 
 export { secureRandomNumber } from "./src/random-digits";
 export { httpResult } from "./src/http-result";
-
+export { buildRouteGenerator } from "./src/route-gen"
 export { StatusCodes as statusCodes } from "http-status-codes";
+export { buildLogger } from "./src/logger"
 
 export * from "aba-utils";
